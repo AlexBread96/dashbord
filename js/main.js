2e9483c8d72929491cd7715a899685cd45504625
Alexandr <63932653+AlexBread96@users.noreply.github.com>
2022-12-25 22:52:22 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,77 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-  $testsList = document.querySelector(".tests-list");
-
+  const $testResult = document.querySelector(".test-result");
+  const $buttonToDashboard = document.querySelector(
+    ".button.button--dashboard"
+  );
   const link = location.href;
-  const maxScore = 7;
-  const colors = {
-    red: "#FF6B62",
-    green: "#A7E92F",
-    yellow: "#FFEB35",
-    blue: "#55B8FF",
-    transparent: "#ffffff00",
-  };
+  let maxScore = 7;
+  let btnLink, score, percantage, curentValue;
+  console.log(link);
+  console.log(link.slice(link.indexOf("last") + 5));
 
-  const dsc = {
-    done: "Subida",
-    middle: "Antes de empezar el viaje",
-    predone: "Conflicto",
-    postNone: "Viaje",
-    none: "Terminacion del viaje",
-  };
-
-  const btns = {
-    done: '<a class="button button--done">Puntaje máximo</a>',
-    restart: '<a class="button button--restart" href=" ">Reiniciar el caso</a>',
-    start: '<a class="button button--new" href=" ">Iniciar el caso</a>',
-    none: '<a class="button button--done"> </a>',
-  };
-  let testsItems = [];
-  let score, percantage;
-
-  function parseLink(link) {
+  function parceLink() {
+    curentValue = link.slice(link.indexOf("last") + 5);
     score = link
       .slice(link.indexOf("score") + 6)
+      .split("&")[0]
       .split("_")
       .filter((el) => el !== "")
-      .map((el) => (+el > maxScore ? 7 : +el));
-    function fillArr(arr) {
-      if (arr.length < 5) {
-        arr.push(0);
-        fillArr(arr);
-      } else {
-        return;
-      }
-    }
+      .map((el) => (+el > maxScore ? maxScore : +el))
+      .filter((el, idx) => idx === (curentValue > 5 ? 4 : curentValue - 1));
+
     fillArr(score);
+    btnLink = `?${link.slice(link.indexOf("fi")).split("&")[0]}&${
+      link.slice(link.indexOf("score")).split("&")[0]
+    }`;
     percantage = score.map((element) =>
       Math.round(((100 / maxScore) * element).toFixed(2))
     );
+    console.log(score);
+    console.log(btnLink);
+    console.log(curentValue);
+    console.log(percantage);
   }
-  function createTestItem(items) {
+
+  function createResult(items) {
+    let item;
     items.forEach((el, i) => {
-      testsItems.push(`<li class="test ${
-        el === maxScore ? "test--disable" : ""
-      }">
-        <section class="test-result ${el === 0 ? "test-result--new" : ""}">
-            <div class="test-diagram test-diagram--small" style="--percentage:${
-              percantage[i]
-            }; --diagram-color:${getColor(i, percantage)};"></div>
-            <div class="test-diagram__description test-diagram__description--small">
-               <p class="test-diagram__number test-diagram__number--small">${el}/${maxScore}</p>
-            </div>
-        </section>
-        <section class="test__description">
-            <p class="test__header">${getDsc(i, score)}</p>
-            ${getBtn(i, score)}
-        </section>
-    </li>`);
-      $testsList.innerHTML += testsItems[i];
+      item = `
+      <div class="test-diagram" style="--percentage:  ${
+        percantage[i]
+      }; --diagram-color:${getColor(i, percantage)};;"></div>
+      <div class="test-diagram__description">
+        <p class="test-diagram__number">${el}/${maxScore}</p>
+        <p class="test-diagram__percent">${
+          percantage[i]
+        }% respuestas correctas</p>
+      </div>
+      `;
     });
+    $testResult.innerHTML = item;
+    $buttonToDashboard.setAttribute(
+      "href",
+      location.origin + "/dashboard.html" + btnLink
+    );
   }
-  parseLink(link);
-  if (link.indexOf("score") !== -1) {
-    createTestItem(score);
-  }
+
+  parceLink();
+  createResult(score);
   function getColor(idx, array) {
     let val = array[idx];
     if (val === 100) {
@@ -86,33 +70,13 @@ document.addEventListener("DOMContentLoaded", function () {
       return colors.red;
     }
   }
-  function getDsc(idx, array) {
-    let val = array[idx];
-    if (val === 0) {
-      return dsc.none;
-    } else if (val >= 1 && val <= 3) {
-      return dsc.postNone;
-    } else if (val >= 4 && val <= 5) {
-      return dsc.middle;
-    } else if (val === 6) {
-      return dsc.predone;
+
+  function fillArr(arr) {
+    if (arr.length < 5) {
+      arr.push(0);
+      fillArr(arr);
     } else {
-      return dsc.done;
-    }
-  }
-  function getBtn(idx, array) {
-    let val = array[idx];
-    if (val === maxScore) {
-      return btns.done;
-    } else if (val > 0) {
-      return btns.restart;
-    } else if (
-      val === 0 &&
-      (array[idx - 1] > 0 || array[idx - 1] === undefined)
-    ) {
-      return btns.start;
-    } else {
-      return "";
+      return;
     }
   }
 });
---
+++ js/tools.js
@@ -0,0 +1,22 @@
+const colors = {
+  red: "#FF6B62",
+  green: "#A7E92F",
+  yellow: "#FFEB35",
+  blue: "#55B8FF",
+  transparent: "#ffffff00",
+};
+
+const dsc = {
+  done: "Subida",
+  middle: "Antes de empezar el viaje",
+  predone: "Conflicto",
+  postNone: "Viaje",
+  none: "Terminacion del viaje",
+};
+
+const btns = {
+  done: '<a class="button button--done">Puntaje máximo</a>',
+  restart: '<a class="button button--restart" href=" ">Reiniciar el caso</a>',
+  start: '<a class="button button--new" href=" ">Iniciar el caso</a>',
+  none: '<a class="button button--done"> </a>',
+};
